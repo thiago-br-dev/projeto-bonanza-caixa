79228c17c7fdd469ef76af816c8dd4dc11519955
thiago-br-dev <thiago_br_dev@outlook.com>
2014-08-29 15:33:10 -0300

    Enviando ...

---
+++ src/view/ChamarCliente.java
@@ -0,0 +1,46 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+
+public class ChamarCliente extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UIManager
+					.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+					ChamarCliente frame = new ChamarCliente();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public ChamarCliente() {
+		setAlwaysOnTop(true);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		setBounds(100, 100, 150, 103);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JButton btnChamarCliente = new JButton("Chamar Cliente");
+		contentPane.add(btnChamarCliente, BorderLayout.CENTER);
+	}
+
+}
