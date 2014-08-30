31a732af45a11b8579f068579f4ef9d9073c7803
thiago-br-dev <thiago_br_dev@outlook.com>
2014-08-29 22:50:30 -0300

    Enviando !

--- src/view/ChamarCliente.java
+++ src/view/ChamarCliente.java
@@ -2,11 +2,22 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+import java.awt.Toolkit;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class ChamarCliente extends JFrame {
 
@@ -42,18 +53,41 @@ public class ChamarCliente extends JFrame {
 	}
 
 	public ChamarCliente() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(ChamarCliente.class.getResource("/view/img/icon_screen.png")));
+		setResizable(false);
 		
 		setAlwaysOnTop(true);
-		setResizable(false);
 		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		setBounds(100, 100, 150, 81);
+		setBounds(100, 100, 161, 147);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
 		JButton btnChamarCliente = new JButton("Chamar Cliente");
-		contentPane.add(btnChamarCliente, BorderLayout.CENTER);
+		btnChamarCliente.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnChamarCliente.setBounds(10, 52, 135, 50);
+		contentPane.add(btnChamarCliente);
+		
+		JComboBox<String> comboBox = new JComboBox<String>();
+		comboBox.setBounds(10, 11, 133, 30);
+		comboBox.setVisible(false);
+		contentPane.add(comboBox);
+		
+		JLabel lblTrocarNmeroDo = new JLabel("Trocar N\u00FAmero do Caixa");
+		lblTrocarNmeroDo.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				
+				comboBox.setVisible(true);
+				lblTrocarNmeroDo.setVisible(false);
+				
+			}
+		});
+		lblTrocarNmeroDo.setBounds(10, 19, 135, 14);
+		contentPane.add(lblTrocarNmeroDo);
+		
+		
 		
 	}
 
